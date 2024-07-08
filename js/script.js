63a185485569787b242dee69a6b7e4cbdc4920be
Franklin <writefranklyn@gmail.com>
2024-07-08 19:00:20 +0100

    add files

---
+++ js/script.js
@@ -0,0 +1,156 @@
+// let Details = {
+//     name: 'Uchenna',
+//     school: 'APTECH COMPUTER EDUCATION',
+//     age: 17,
+//     gender: 'Male',
+//     hobbies: ['Reading',' Coding', ' and Fighting Karate'],
+//     friend: 'Aninna',
+
+
+//     greet: function(){
+//         console.log("Good morning" + ' ' + this.name)
+//     },
+
+//     composition: function(){
+//         console.log(`My name is ${this.name}, I school in ${this.school}, I am ${this.age} and i'm a ${this.gender}, My hobbies are ${this.hobbies}, and my best friend is ${this.friend}.`)
+//     }
+
+
+// };
+
+// Details.greet();
+// Details.composition();
+
+// document.querySelector('#f1').innerHTML= '<H2>MEAT</H2>';
+
+// document.querySelector('#f2').innerHTML = '<H2>DRINK</H2>';
+
+// document.querySelector('#f3').innerHTML = '<H2>DESERT</H2>';
+
+
+// let nam= 'john';
+
+// if (nam === 'john'){
+//     console.log('Your name is John')
+// }else{
+//     console.log('You are not John')
+// }
+
+
+// let age = prompt("Enter your age");
+
+// if (age > 18){
+//     console.log('Qualified for Driving')
+// }else{
+//     console.log('NOT QUALIFIED')
+// }
+
+
+// let book = prompt("Enter your book");
+
+// if (book == 'Maths'){
+//     console.log("Maths is available")
+// }else if(book == "History"){
+//     console.log("History is available")
+// }else if(book == "English"){
+//     console.log("English is available")
+// }else[
+//     console.log("Books not available")
+// ]
+
+// let grade = prompt("Enter your grade");
+
+// if (grade == 'A'){
+//     console.log('Good Job');
+// }else if(grade == 'B'){
+//     console.log('Pretty Good job');
+// }else if(grade == 'C'){
+//     console.log("You passed");
+// }else{
+//     console.log("You failed")
+// }
+
+
+// switch(grade){
+//     case 'A':
+//         console.log("Good Job");
+//         break;
+//     case 'B':
+//         console.log("Credit");
+//         break;
+//     case 'C':
+//         console.log('You passed');
+//         break;
+//     default:
+//         console.log('You failed');
+//         break;
+// }
+
+
+// let i = 0;
+
+// while(i <= 10){
+//     console.log(i);
+//     i++;
+// }
+
+// do {
+//     console.log(i);
+//     i++; 
+// }while(i <= 10)
+
+// for(let i = 0; i <= 10; i++){
+//     console.log(i);
+// }
+
+// let food = ['Rice', 'Yam', 'Water', 'Stew', 'Beans'];
+
+// for(let i = 0; i < food.length; i++){
+//     console.log(food[i]);
+// }
+
+// function sayHello(){
+//     alert("Hello Welcome to Aptech")
+// }
+
+// function sumoftwo(x, y){
+//     console.log(x + y);
+// }
+
+
+// document.querySelector('#name').innerHTML = 'Uche';
+
+// document.getElementById('name').innerHTML = '<i>Uchenna</i>'
+
+// function sumOfTwo(){
+//     let a = document.getElementById('num1').value;
+//     let b = document.getElementById('num2').value;
+
+//     document.write(+a + +b);
+// }
+// function minusOfTwo(){
+//     let a = document.getElementById('num1').value;
+//     let b = document.getElementById('num2').value;
+
+//     document.write(+a - +b);
+// }
+// function multiplicationOfTwo(){
+//     let a = document.getElementById('num1').value;
+//     let b = document.getElementById('num2').value;
+
+//     document.write(+a * +b);
+// }
+// function divisionOfTwo(){
+//     let a = document.getElementById('num1').value;
+//     let b = document.getElementById('num2').value;
+
+//     document.write(+a / +b);
+// }
+
+// function talk(){
+//     document.write("these are your books");
+// }
+
+// let h2 = document.querySelector('h2');
+
+// h2.addEventListener('click', talk);
